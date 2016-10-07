868ac4dbaa99d4b768fe28d9e00203e6d52438fa
keitheous <keithchongwy@gmail.com>
2016-10-07 14:20:45 +1100

    read me and seed.rb file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create(name: "admin", password: "abc123")
+Contact.create(name: "Keith", email: "keithchongwy@gmail.com", phone: "0420 325 425", msg: "please inform me your thoughts and what you need changed.")
